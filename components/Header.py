85c1065c83f2c22183f39cbc379df51ec3e80c75
NunungAliMaulana <nunungalimaulana@students.unnes.ac.id>
2024-07-13 16:48:45 +0700

    fix:update main menu

--- components/Header.py
+++ components/Header.py
@@ -11,10 +11,9 @@ def header():
                 Hai 👋, Selamat Datang di <strong>RiceClassify!</strong>
                 <i>Website</i> yang dapat membantumu mengklasifikasikan beras ke dalam 2 varietas (<i><strong>Cammeo</strong></i> dan <i><strong>Osmancik</strong></i>) dengan mudah. Untuk menggunakannya, caranya cukup mudah, ikuti tutorial berikut ini ya...<br>
                 <ol>
-                        <li>Untuk memulai, klik tombol <strong>Klasifikasi</strong> di bawah ini untuk menuju halaman "Klasifikasi".
-                        <li>Pada halaman <strong>Klasifikasi</strong>, <i>input</i> nilai setiap <i>form</i> yang merupakan ciri - ciri dari beras</li>
-                        <li>Terakhir, klik tombol <strong>PROSES</strong> dan ... Simsalabim, Model akan mengolah inputanmu dan kamu akan mendapatkan hasil klasifikasinya</li>
-                        <li>Terakhir, klik tombol <strong>PROSES</strong> dan ... Simsalabim, Model akan mengolah inputanmu dan kamu akan mendapatkan hasil klasifikasinya</li>
+                        <li>Untuk memulai, klik tombol <strong>Klasifikasi</strong> di bawah ini.
+                        <li>Pada halaman <strong>Klasifikasi</strong>, <i>input</i> nilai setiap <i>form</i> sesuai rentang yang telah ditentukan.
+                        <li>Terakhir, klik tombol <strong>Hasil Klasifikasi</strong> dan ... Simsalabim, Model akan mengolah inputanmu dan akan menunjukkan hasil klasifikasinya</li>
                 </ol>
                 </p>
                 ''', 
